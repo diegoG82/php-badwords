e7c5c41e9976983f01694eb3249017be562b701e
diegoG82 <diegogiardina@hotmail.it>
2023-05-03 15:35:20 +0200

    css added

--- badwords.php
+++ badwords.php
@@ -3,8 +3,6 @@ $paragraph = $_GET['paragraph'];
 $censoredword = $_GET['censoredword'];
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,32 +10,45 @@ $censoredword = $_GET['censoredword'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Censored Badwords</title>
+    <!-- Main Styles -->
+    <link rel="stylesheet" href="./css/style.css" />
+
 </head>
 
 <body>
-    <div class="original">
-        <h2>ORIGINAL PARAGRAPH</h2>
-        <p><?php echo "$paragraph"; ?></p>
+    <div class="main">
 
-        <p> il paragrafo e' lungo <?php
-                                    echo strlen("$paragraph");
-                                    ?> caratteri</p>
-    </div>
+        <div class="original">
+            <h2>ORIGINAL PARAGRAPH</h2>
+            <p><?php echo "$paragraph"; ?></p>
+
+            <p> il paragrafo e' lungo <?php
+                                        echo strlen("$paragraph");
+                                        ?> caratteri</p>
+        </div>
 
 
-    <div class="censored">
-        <h2>CENSORED PARAGRAPH</h2>
-        <?php
+        <div class="censored">
+            <h2>CENSORED PARAGRAPH</h2>
+            <p><?php
+                $paragraph_replace = str_replace($censoredword, '***', $paragraph);
+                echo $paragraph_replace;
 
-        $paragraph_replace = str_replace($censoredword, '***', $paragraph);
-        echo $paragraph_replace;
 
-        ?>
-        <p>Il paragrafo è ora lungo <?php
-                                    echo strlen("$paragraph_replace");
-                                    ?> caratteri </p>
-    </div>
+
+                ?></p>
+            <p>Il paragrafo è ora lungo <?php
+                                        echo strlen("$paragraph_replace");
+                                        ?> caratteri </p>
+        </div>
+
+
+
+
+
+
+
 
 
 
